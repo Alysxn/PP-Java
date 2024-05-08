2022cbdee96c6ac53cc7c4bcd13e6e8c7872453b
Alyson <alyson@gmail.com>
2024-05-08 13:27:11 -0400

    novo exercicio

---
+++ Filmes.java
@@ -0,0 +1,48 @@
+public class Filmes{
+        String titulo;
+        String genero;
+        String idioma;
+        String sinopse;
+        String plataformaDisponivel;
+        int anoDeLancamento;
+        int faixaEtaria;
+
+
+        public void inserirTitulo(String tituloFilme){
+            titulo = tituloFilme;
+        }
+        public void inserirGenero(String generoFilme){
+            genero = generoFilme;
+        }
+        public void inserirIdioma(String idiomaFilme){
+            idioma = idiomaFilme;
+        }
+        public void inserirSinopse(String sinopseFilme){
+            sinopse = sinopseFilme;
+        }
+        public void inserirPlataformaDisponivel(String plataformaDisponivelFilme){
+            plataformaDisponivel = plataformaDisponivelFilme;
+        }
+
+        public void inserirAnoDeLancamento(int anoDeLancamentoFilme){
+            anoDeLancamento = anoDeLancamentoFilme;
+        }
+        public void inserirfaixaEtaria(int faixaEtariaFilme){
+            faixaEtaria = faixaEtariaFilme;
+        }
+
+
+
+        public void imprimeDados(){
+            System.out.println("Muito obrigado! Segue as informações completas do seu filme favorito: \n");
+            System.out.println("Titulo: " + titulo);
+            System.out.println("Genero: " + genero);
+            System.out.println("Idioma orinal: " + idioma);
+            System.out.println("Sinopse: " + sinopse);
+            System.out.println("Plataforma disponivel: " + plataformaDisponivel);
+            System.out.println("Ano de lancamento: " + anoDeLancamento);
+            System.out.println("faixaEtaria: " + faixaEtaria);
+        }
+}
+
+
--- Main.java
+++ Main.java
@@ -1,5 +1,43 @@
-public class Main{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("teste");
+        Scanner scanner = new Scanner(System.in);
+        Filmes novoFilme = new Filmes();
+        String informacoesFilmes;
+
+        System.out.println("Digite os dados do seu filme favorito!\n");
+
+        System.out.println("Digite o título do filme: ");
+        informacoesFilmes = scanner.nextLine();
+        novoFilme.inserirTitulo(informacoesFilmes);
+
+        System.out.println("Digite o gênero do filme: ");
+        informacoesFilmes = scanner.nextLine();
+        novoFilme.inserirGenero(informacoesFilmes);
+
+        System.out.println("Digite o idioma original do filme: ");
+        informacoesFilmes = scanner.nextLine();
+        novoFilme.inserirIdioma(informacoesFilmes);
+
+        System.out.println("Digite a sinopse do filme: ");
+        informacoesFilmes = scanner.nextLine();
+        novoFilme.inserirSinopse(informacoesFilmes);
+
+        System.out.println("Digite a plataforma disponivel do filme: ");
+        informacoesFilmes = scanner.nextLine();
+        novoFilme.inserirPlataformaDisponivel(informacoesFilmes);
+
+        System.out.println("Digite o ano de lançamento do filme: ");
+        int anoLancamento = scanner.nextInt();
+        novoFilme.inserirAnoDeLancamento(anoLancamento);
+
+        System.out.println("Digite a faixa etária do filme: ");
+        int faixaEtaria = scanner.nextInt();
+        novoFilme.inserirfaixaEtaria(faixaEtaria);
+
+        novoFilme.imprimeDados();
+
+        scanner.close(); 
     }
 }
